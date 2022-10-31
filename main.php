347c291c3456685ecdc2e405082faebe471c40f4
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-31 09:39:07 -0700

    Add files via upload

---
+++ main.php
@@ -0,0 +1,82 @@
+// Marvish Chandra
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Best Selling Candy Reccomendations</h1>
+
+<?php
+$firstItem = "Snickers Fun Size Chocolate Candy Bars";
+$firstItemprice = 4.19;
+echo $firstItem , $firstItemprice;
+$secondItem = "Dum Dum Original Assorted Flavors Lollipops - 13 oz";
+$secondItemprice = 3.79;
+echo $secondItem, $secondItemprice
+$thirdItem = "Reese's Take 5 Pretzel, Caramel, Peanut Butter, Chocolate Snacks";
+$thirdItemprice = 4.19;
+echo $thirdItem, $thirdItemprice
+$fourthItem = "Kit Kat Milk Chocolate Snack Size Wafer Candy Bars - 10.78oz";
+$fourthItemprice = 4.19;
+echo $fourthItem, $fourthItemprice
+$fifthItem = "Twix Fun Size Caramel Chocolate Cookie Bar Candy - 10.83oz";
+$fifthItemprice = 4.19;
+echo $fifthItem, $fifthItemprice
+$sixthItem = "Smarties Assorted Flavors Candy Rolls - 18oz";
+$sixthItemprice = 3.29
+echo $sixthItem, $sixthItemprice
+$seventhItem = "M&M's Fun Size Milk Chocolate Candies - 10.53oz";
+$seventhItemprice = 4.19;
+echo $seventhItem, $seventhItemprice
+$eighthItem = "Mars Fun Size Chocolate Favorites Variety Pack - 30.98oz";
+$eighthItemprice = 10.99
+echo $eighthItem, $eighthItemprice
+$ninthItem = "100 Grand Fun Size Chocolate Candy - 10oz";
+$ninthItemprice = 4.19;
+echo $ninthItem, $ninthItemprice
+$tenthItem = "Skittles and Starburst Fun Size Mix Bite Size Candies";
+$tenthItemprice = 9.99;
+echo $tenthItem, $tenthItemprice
+$eleventhItem = "Milky Way Fun Size Milk Chocolate Candy Bars - 10.65oz";
+$eleventhItemprice = 4.19;
+echo $eleventhItem, $eleventhItemprice
+$twelvthItem = "Mars Minis Chocolate Favorites Variety Pack - 125ct/35.24oz";
+$twelvthItemprice = 10.99;
+echo $twelvthItem, $twelvthItemprice
+$thirteenthItem = "Butterfinger Fun Size Chocolate Bar 10.2oz Bag";
+$thirteenthItemprice = 4.19;
+echo $thirteenthItem, $thirteenthItemprice
+$fourteenthItem = "M&M's Peanut Fun Size Chocolate Candies - 10.57oz";
+$fourteenthItemprice = 4.19;
+echo $fourteenthItem, $fourteenthItemprice
+$fifteenthItem = "Hershey's Kisses Milk Chocolate Candy -35.8oz";
+$fifteenthItemprice = 10.99;
+echo $fifteenthItem, $fifteenthItemprice
+$sixteenthItem = "Crunch Fun Size Chocolate Bar - 10oz Bag";
+$sixteenthItemprice = 4.19;
+echo $sixteenthItem, $sixteenthItemprice
+$seventeenthItem = "Albanese Worlds Best 12 Flavor Gummi Bears – 36 oz";
+$seventeenthItemprice = 7.49;
+echo $seventeenthItem, $seventeenthItemprice
+$eighteenthItem = "Almond Joy Coconut and Almond Chocolate Snack Size Candy Bar - 11.3oz";
+$eighthItemprice = 4.19;
+echo $eighteenthItem, $eighthItemprice
+$ninteenthItem = "Airheads Assorted Mini Bars - 14oz";
+$ninteenthItemprice = 3.69;
+echo $ninteenthItem, $ninteenthItemprice
+$twentythItem = "Reese's and Kit Kat Miniatures Milk Chocolate Assortment Candy Variety Pack -33.36 oz";
+$twentythitemprice = 10.99;
+echo $twentyfirstItem, $twentythirditemprice
+$twentyfirstItem = "Jolly Rancher Assorted Fruit Flavored Hard Candy - 60oz";
+$twentyfirstItemprice = 8.99;
+echo $twentyfirstItem, $twentyfirstItemprice
+$twentysecondItem = "Reese's Miniatures Milk Chocolate Peanut Butter Cups - 35.6oz";
+$twentysecondItemprice = 10.99;
+echo $twentysecondItem, $twentysecondItemprice
+$twentythirdItem = "Hershey's Miniatures Assorted Chocolate Candy Bars - 35.9oz";
+$twentythirditemprice = 10.99;
+echo $twentythirdItem, $twentythirditemprice
+$twentyfourthItem = "Mars Chocolate Caramel Lovers Variety Pack - 30.98oz";
+$twentyfourthItemprice = 10.99;
+echo $twentyfourthItem, $twentyfourthItemprice
+?>
